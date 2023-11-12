8542406e4ac3dae3b4126e5f183c781297e73dff
Nickoscos <nicolas.pelleschi@gmail.com>
2023-11-12 15:08:41 +0100

    Création de la database Activation de la lecture de tous les pr

--- HPlusSport.API/Controllers/ProductController.cs
+++ HPlusSport.API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HPlusSport.API.Controllers
@@ -7,9 +8,17 @@ namespace HPlusSport.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ShopContext _context;
+
+        public ProductsController (ShopContext context) 
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
         [HttpGet]
-        public string GetProducts() {
-            return "OK.";
+        public IEnumerable<Product> GetAllProducts() {
+            return _context.Products.ToArray();
         }
 
     }
